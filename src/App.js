e340b4d27b62afb7110dbd7ac38bfd7f00a94591
stefani.stuparic <75335116+stefanistuparic@users.noreply.github.com>
2022-10-25 20:08:39 +0200

    Final assignment for Algebra
    This is my final assignment for Algreba.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Room from './Components/Room'
+function App() {
+  return (
+    <div className='app'>
+      <Room/>
+    </div>
+   
+  );
+}
+
+export default App;
